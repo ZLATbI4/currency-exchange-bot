693fcdd1f7e2f507db671849153993e3b8b889ff
Yevhenii Zlatov <zlatych@MacBook-Pro.local>
2020-11-24 00:44:01 +0200

    run script modified

--- start.sh
+++ start.sh
@@ -1,4 +1,24 @@
 #!/bin/bash
 
-docker build -t curbot .
-docker run --restart=unless-stopped -d --name curbot curbot
+# if container not exists or stopped
+if [ ! "$(docker ps -q -f name=curbot)" ]; then
+    if [ "$(docker ps -aq -f status=exited -f name=curbot)" ]; then
+        # cleanup
+        docker rm curbot
+        docker system prune -f
+    fi
+    # run your container
+    docker build -t curbot .
+    docker run --restart=unless-stopped -d --name curbot curbot
+fi
+# if container exists
+if [ $(docker ps -q -f name=curbot) ]; then
+  # cleanup
+  docker stop curbot
+  docker rm curbot
+  docker system prune -f
+  docker rmi curbot
+  # run your container
+  docker build -t curbot .
+  docker run --restart=unless-stopped -d --name curbot curbot
+fi
